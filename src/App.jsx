18364d868596907c4bdc00a9df5a23dd58152306
Lucija <lucia.hranjec@gmail.com>
2023-01-22 16:29:42 +0100

    Fixed fetch, using hosted json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Component } from "react";
+import { getData } from "./services/fetch";
 
 class App extends Component {
-
     state = {
+        isLoaded: false,
+        questions: [],
+        memes: [],
+        tryAgainMeme: "",
+    };
 
+    componentDidMount() {
+        const data = getData();
+        this.setState(() => ({questions: data.questions, memes: data.memes, tryAgainMeme: data.tryAgainMeme}),
+        () => console.log(this.state))
     };
 
     render() {
-        return (<></>);
+        return <></>;
     }
 }
 
